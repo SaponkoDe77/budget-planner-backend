5af4fab1b4c76e9a2b51c3b2aabd79a4f7199d17
Denis Saponko <denisok23071998@gmail.com>
2023-11-29 08:03:04 +0200

    add configuration files

---
+++ src/config/configuration.ts
@@ -0,0 +1,14 @@
+export default () => ({
+  app: {
+    port: parseInt(process.env.PORT) || 3001,
+  },
+  database: {
+    type: 'postgres',
+    port: parseInt(process.env.DB_PORT) || 5433,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
+    synchronize: true,
+  },
+});
